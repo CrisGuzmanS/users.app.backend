939e9f55dc98b5450e31a65e20c8b6bf2525178d
Cristian Guzmán <cristian.guzman.contacto@gmail.com>
2024-02-04 18:56:20 +0000

    TokenService.js

--- src/Users/Controllers/indexUserController.js
+++ src/Users/Controllers/indexUserController.js
@@ -1,5 +1,15 @@
 import { User } from '../Models/User.js';
+import { TokenService } from '../../Auth/Services/TokenService.js';
 
 export const indexUserController = async (request, response) => {
+
+    const token = request.headers.authorization;
+
+    if (!TokenService.isValid(token)) {
+        return response.status(403).json({
+            message: 'Credenciales inválidas'
+        });
+    }
+
     response.json(await User.findAll());
 }
