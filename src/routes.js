684d4feebadb04e0478fd9bd8de6893c66f2588c
Cristian Guzmán <cristian.guzman.contacto@gmail.com>
2024-02-05 03:18:32 +0000

    update user

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import { createUserController } from './Users/Controllers/createUserController.j
 import { loginController } from './Auth/Controllers/loginController.js';
 import { deleteController } from './Auth/Controllers/deleteController.js';
 import { showController } from './Auth/Controllers/showController.js';
+import { updateController } from './Auth/Controllers/updateController.js';
 import { verifyTokenController } from './Auth/Controllers/verifyTokenController.js';
 import { tokenMiddleware } from './Auth/Middlewares/tokenMiddleware.js';
 
@@ -15,5 +16,6 @@ router.get('/api/v1/usuarios', tokenMiddleware, indexUserController);
 router.post('/api/v1/usuarios', tokenMiddleware, createUserController);
 router.delete('/api/v1/usuarios/:id', tokenMiddleware, deleteController);
 router.get('/api/v1/usuarios/:id', tokenMiddleware, showController);
+router.patch('/api/v1/usuarios/:id', tokenMiddleware, updateController);
 
 export { router };
